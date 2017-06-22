f6b8a6829505e2acc28b8c3fba20f2a312cb291c
Yaroslav Yermilov <yermilov17@gmail.com>
2017-06-22 09:53:39 +0300

    #2. fix issue with 404

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'core-js/fn/object/assign';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { makeMainRoutes } from './routes';
 
 const routes = makeMainRoutes();
