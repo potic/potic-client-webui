dab0d8ddb3fe74d581d88a545b3ca1a33a984f13
Yaroslav Yermilov <yermilov17@gmail.com>
2017-08-29 02:06:54 +0300

    #8. make POST call to mark article as read

--- src/config/base.js
+++ src/config/base.js
@@ -6,6 +6,7 @@ export default {
   auth0_clientId: 'NtKWNf6V7S_gmV50Cz4zKJVAaAeNPxwe',
   auth0_audience: 'potic-aggregator',
 
-  services_aggregator: 'http://185.14.185.186:40401'
+  services_aggregator: 'http://185.14.185.186:40401',
+  services_articles: 'http://185.14.185.186:40402'
 
 }
