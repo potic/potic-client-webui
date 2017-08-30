3a43825c533304be5b0e6ce5d83a9a4f12090d07
Yaroslav Yermilov <yermilov17@gmail.com>
2017-08-31 00:44:32 +0300

    #8. update mark as read API calls according to latest changes

--- src/config/dev.js
+++ src/config/dev.js
@@ -5,7 +5,10 @@ import baseConfig from './base';
 let config = {
   appEnv: 'dev',
 
-  auth0_callbackUrl: 'http://localhost:8000/callback'
+  auth0_callbackUrl: 'http://localhost:8000/callback',
+
+  services_aggregator: 'http://localhost:40401',
+  services_articles: 'http://localhost:40402'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
