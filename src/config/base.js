e6015fbd0b99b41b5a8a331dc73da82c84a01a01
Yaroslav Yermilov <yermilov17@gmail.com>
2017-10-06 01:09:50 +0300

    #19. fix PROD instance uses TEST backend

--- src/config/base.js
+++ src/config/base.js
@@ -8,9 +8,6 @@ export default {
   auth0_clientId: 'NtKWNf6V7S_gmV50Cz4zKJVAaAeNPxwe',
   auth0_audience: 'potic-api',
   auth0_response: 'token id_token',
-  auth0_scope: 'openid profile get:articles update:articles',
-
-  services_aggregator: 'http://185.14.185.186:40401',
-  services_articles: 'http://185.14.185.186:40402'
+  auth0_scope: 'openid profile get:articles update:articles'
 
 }
--- src/config/dist.js
+++ src/config/dist.js
@@ -5,6 +5,9 @@ import baseConfig from './base';
 let config = {
   appEnv: 'dist',
 
+  services_aggregator: 'http://46.101.62.243:40401',
+  services_articles: 'http://46.101.62.243:40402',
+
   auth0_callbackUrl: 'http://46.101.62.243/callback'
 };
 
