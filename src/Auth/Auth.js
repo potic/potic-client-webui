bdf2ff8f0a81ffc2dc24a8de019fbec5feffa45d
Yaroslav Yermilov <yermilov17@gmail.com>
2017-11-28 01:28:31 +0200

    #26. try to use postMessageOrigin parameter

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -102,7 +102,8 @@ export default class Auth {
       {
         audience: config.auth0_audience,
         redirectUri: config.auth0_tokenRenewerUrl,
-        usePostMessage: true
+        usePostMessage: true,
+        postMessageOrigin: config.auth0_tokenRenewerUrl
       },
       (err, result) => {
         if (err) {
