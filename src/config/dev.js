89bf1f35c8df8c3985080f4af1f922302522659d
Yaroslav Yermilov <yermilov17@gmail.com>
2017-12-27 00:38:20 +0200

    #33. prototype stub for logging

--- src/config/dev.js
+++ src/config/dev.js
@@ -5,6 +5,7 @@ import baseConfig from './base';
 let config = {
 
   appEnv: 'dev',
+  env: 'dev',
 
   title: 'potic.dev',
 
@@ -12,6 +13,8 @@ let config = {
   services_sections: 'http://185.14.185.186:40401',
   //services_articles: 'http://localhost:40402',
   services_articles: 'http://185.14.185.186:40402',
+  //services_logger: 'http://localhost:40410',
+  services_logger: 'http://185.14.185.186:40410',
 
   auth0_callbackUrl: 'http://localhost:8000/callback',
   auth0_tokenRenewerUrl: 'http://localhost:40407'
--- src/config/dist.js
+++ src/config/dist.js
@@ -3,10 +3,13 @@
 import baseConfig from './base';
 
 let config = {
+
   appEnv: 'dist',
+  env: 'prod',
 
   services_sections: 'http://46.101.62.243:40401',
   services_articles: 'http://46.101.62.243:40402',
+  services_logger: 'http://46.101.62.243:40410',
 
   auth0_callbackUrl: 'http://46.101.62.243/callback',
   auth0_tokenRenewerUrl: 'http://46.101.62.243:40407'
