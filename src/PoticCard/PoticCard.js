de8574360c9e7398dc493a6a055c2c2537d037f2
Yaroslav Yermilov <yermilov17@gmail.com>
2018-05-11 09:57:54 +0300

    #35. improve how time is shown in cards

--- src/PoticCard/PoticCard.js
+++ src/PoticCard/PoticCard.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import React from 'react';
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
 const styles = {
   card: {
     margin: 5,
     width: 300,
-    minWidth: 300,
+    minWidth: 300
   },
   cardInner: {
     display: 'flex',
     flexDirection: 'column',
     height: 480,
     justifyContent: 'flex-end',
-    overflowY: 'hidden',
+    overflowY: 'hidden'
   },
   media: {
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    overflowY: 'hidden',
+    overflowY: 'hidden'
   },
   title: {
-    lineHeight: 1,
+    lineHeight: 1
   },
   link: {
-    textDecoration:'none',
+    textDecoration:'none'
   },
 };
 
@@ -45,16 +45,8 @@ class PoticCard extends React.Component {
           </CardMedia>
 
           <a href={this.props.post.url} style={styles.link} target="_blank">
-            <CardTitle title={this.props.post.title} subtitle={this.props.post.source} titleStyle={styles.title} />
+            <CardTitle title={this.props.post.title} subtitle={this.props.post.source + this.timestampToString(this.props.post.addedTimestamp)} titleStyle={styles.title} />
           </a>
-
-          {
-            (this.props.post.addedTimestamp != null) && (
-              <CardText>
-                Added {new Date(this.props.post.addedTimestamp * 1000).toLocaleDateString()}
-              </CardText>
-            )
-          }
 
           <CardActions>
             <FlatButton label="like" onClick={() => this.props.onMarkLiked(this.props.post.id)} />
@@ -65,6 +57,37 @@ class PoticCard extends React.Component {
       </Card>
     );
   }
-};
+
+  timestampToString(timestamp) {
+    if (timestamp == null) {
+      return '';
+    }
+
+    const secondsAgo = new Date().getTime() / 1000 - timestamp;
+
+    const minutesAgo = secondsAgo / 60;
+    if (minutesAgo < 60) {
+      return ` · ${Math.floor(minutesAgo)} minute${Math.floor(minutesAgo) > 1 ? 's' : ''} ago`;
+    }
+
+    const hoursAgo = minutesAgo / 60;
+    if (hoursAgo < 24) {
+      return ` · ${Math.floor(hoursAgo)} hour${Math.floor(hoursAgo) > 1 ? 's' : ''} ago`;
+    }
+
+    const daysAgo = hoursAgo / 24;
+    if (daysAgo < 30) {
+      return ` · ${Math.floor(daysAgo)} day${Math.floor(daysAgo) > 1 ? 's' : ''} ago`;
+    }
+
+    const monthsAgo = daysAgo / 30;
+    if (monthsAgo < 12) {
+      return ` · ${Math.floor(monthsAgo)} month${Math.floor(monthsAgo) > 1 ? 's' : ''} ago`;
+    }
+
+    const yearsAgo = monthsAgo / 12;
+    return ` · ${Math.floor(yearsAgo)} year${Math.floor(yearsAgo) > 1 ? 's' : ''} ago`;
+  }
+}
 
 export default PoticCard;
